6cb28e1c078e36bcbf728fe9d4fbd5b2345bb062
takuya1 <oktkgm1997@icloud.com>
2026-04-24 15:41:10 +0900

    feat: トレーニングのバリデーションを追加

--- app/models/training.rb
+++ app/models/training.rb
@@ -1,4 +1,18 @@
 class Training < ApplicationRecord
   belongs_to :user
   belongs_to :target
+
+  validates :theme, presence: true
+  validates :explanation, presence: true, length: { minimum: 10 }
+  validates :target_id, presence: true
+
+  validate :custom_target_if_needed
+
+  private
+
+  def custom_target_if_needed
+    if target&.name == "その他" && custom_target.blank?
+      errors.add(:custom_target, "を入力してください")
+    end
+  end
 end
