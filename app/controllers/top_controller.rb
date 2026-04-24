9c7621a17ed736d2647abde608005022ba45e10f
takuya1 <oktkgm1997@icloud.com>
2026-04-24 13:15:25 +0900

    fix: authenticate_user!対応としてテストにsign_inを追加

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,4 @@
 class TopController < ApplicationController
-  before_action :authenticate_user!
 
   def index
   end
--- test/controllers/trainings_controller_test.rb
+++ test/controllers/trainings_controller_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class TrainingsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+    sign_in @user
+  end
+
   test "should get index" do
     get trainings_url
     assert_response :success
