40753b3fccffa839b0453321aaa4c885c9160400
takuya1 <oktkgm1997@icloud.com>
2026-04-24 14:22:37 +0900

    feat: targetの初期データを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+targets = ["小学生", "大人", "初心者", "ベテラン", "その他"]
+
+targets.each do |name|
+  Target.find_or_create_by!(name: name)
+end
