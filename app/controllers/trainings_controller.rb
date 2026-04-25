247bfb9cabd8b58560d32d7c2f24c272af54b622
takuya1 <oktkgm1997@icloud.com>
2026-04-25 20:17:04 +0900

    feat: トレーニング履歴取得処理を実装

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -2,6 +2,7 @@ class TrainingsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @trainings = current_user.trainings.order(created_at: :desc)
   end
 
   def new
